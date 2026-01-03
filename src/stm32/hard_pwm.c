94269679958076d81ba55e33a31fdd9c98d0007f
WojciechBednarski <11602670+WojciechBednarski@users.noreply.github.com>
2026-01-03 23:13:31 +0100

    revert

--- src/stm32/hard_pwm.c
+++ src/stm32/hard_pwm.c
@@ -24,9 +24,6 @@ static const struct gpio_pwm_info pwm_regs[] = {
   #if CONFIG_MACH_STM32F0x2
     {TIM3, GPIO('B', 4), 1, GPIO_FUNCTION(1)},
   #endif
-  #if CONFIG_MACH_STM32F042
-    {TIM3, GPIO('B', 4), 1, GPIO_FUNCTION(1)},
-  #endif
   #if CONFIG_MACH_STM32F070
     {TIM15, GPIO('A', 2), 1, GPIO_FUNCTION(0)},
     {TIM15, GPIO('A', 3), 2, GPIO_FUNCTION(0)},
